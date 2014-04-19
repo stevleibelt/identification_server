a401c2052e810b7036111dbf9790f9c6f2894fac
stevleibelt <stev@leibelt.de>
2014-04-20 00:36:13 +0200

    added identity factory

--- application/Service/Factory/FileDatabaseFactory.php
+++ application/Service/Factory/FileDatabaseFactory.php
@@ -22,9 +22,11 @@ class FileDatabaseFactory implements FactoryInterface
         $pathToDatabase = realpath(__DIR__ . '/../../../data/dynamic/database.php');
         $data = (is_readable($pathToDatabase)) ? require_once $pathToDatabase : array();
         $hasherFactory = new HasherFactory();
+        $identityFactory = new IdentityFactory();
 
         $database = new FileDatabase($data);
         $database->setHasher($hasherFactory->create());
+        $database->setIdentityFactory($identityFactory);
 
         return $database;
     }
